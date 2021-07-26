93856d13fe243a4ad6dd47803aa5e4e756aaf025
Jaqueline Silva <jaqueline.natario@gmail.com>
2021-07-26 12:07:08 -0300

    Cria o arquivo Component.js

--- 2-desenvolvimento-front-end/bloco-11-introducao-a-react/dia-1-introducao-front-end-e-react-e-hello-world/conteudo/first-react-app/src/App.js
+++ 2-desenvolvimento-front-end/bloco-11-introducao-a-react/dia-1-introducao-front-end-e-react-e-hello-world/conteudo/first-react-app/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Component from './Component';
 import HelloWorld from './HelloWorld';
+
 
 function App() {
   return (
-    <HelloWorld />
-  );
+    <div>
+      <HelloWorld />
+      <Component />
+    </div>
+  )
 }
 
 export default App; // estamos exportando esse arquivo para index.js, onde será renderizado por meio do ReactDOM.render
---
+++ 2-desenvolvimento-front-end/bloco-11-introducao-a-react/dia-1-introducao-front-end-e-react-e-hello-world/conteudo/first-react-app/src/Component.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Component extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Jaqueline</h1>
+        <p>Sou aluna de desenvolimento Web na Trybe e estou aprendendo React. Estou gostando muito desse novo aprendizado.</p>
+      </div>
+    )
+  }
+}
+
+export default Component;
