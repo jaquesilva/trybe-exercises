3544200eb0013e14427ab4094bd8936a79691f44
Jaqueline Silva <jaqueline.natario@gmail.com>
2021-07-30 14:29:06 -0300

    Realiza algumas alterações

---
+++ 2-desenvolvimento-front-end/bloco-13-ciclo-de-vida-de-componentes-e-react-router/dia-1-ciclo-de-vida-de-componentes/conteudo/my-interdimensional-app/src/App.js
@@ -0,0 +1,47 @@
+// App.js
+import React, { Component }from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+        characters: [],
+    };
+  }
+
+  fetchCharacters() {
+    fetch('https://rickandmortyapi.com/api/character')
+    .then(response => response.json())
+    .then(data => {
+      this.setState({characters: data.results})
+    })
+  }
+
+  componentDidMount() {
+    this.fetchCharacters();
+  }
+
+  render() {
+    const { characters } = this.state;
+    return (
+      <div className="App">
+        <h1>
+          Ricky and Morty Characters:
+        </h1>
+        <div className="body">
+          {characters.map(({ name, image }) => {
+            return (
+              <div className="container" key={name}> {/* a função das keys é ajudar o React a identificar quais itens sofreram alterações para que o React não precise renderizar novamente toda a lista novamente e sim apenas se preocupar com a parte modificada. */}
+                <h3>{name}</h3>
+                <img src={image} alt={name}/>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
