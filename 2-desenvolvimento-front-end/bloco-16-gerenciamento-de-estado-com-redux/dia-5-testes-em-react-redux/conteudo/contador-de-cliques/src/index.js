f048a6ca228620a60402023e244b7867506ed000
Jaqueline Silva <jaqueline.natario@gmail.com>
2021-08-28 15:27:36 -0300

    Praticando o conteúdo do course

---
+++ 2-desenvolvimento-front-end/bloco-16-gerenciamento-de-estado-com-redux/dia-5-testes-em-react-redux/conteudo/contador-de-cliques/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+// Store provido pela nossa aplicação. Nos testes, precisamos prover um novo store para podermos controlá-lo
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store} >
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+serviceWorker.unregister();
---
+++ 2-desenvolvimento-front-end/bloco-16-gerenciamento-de-estado-com-redux/dia-5-testes-em-react-redux/conteudo/contador-de-cliques/src/store/index.js
@@ -0,0 +1,11 @@
+// 4
+
+// src/store/index.js
+import { createStore, combineReducers } from 'redux';
+import clickReducer from '../reducers';
+
+const rootReducer = combineReducers({ clickReducer });
+
+const store = createStore(rootReducer);
+
+export default store;
