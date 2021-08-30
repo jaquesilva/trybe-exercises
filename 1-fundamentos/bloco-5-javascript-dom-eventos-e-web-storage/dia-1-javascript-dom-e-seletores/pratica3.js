3cf351e6da6557194797f85b27dd936e8d798d8b
Jaqueline Silva <jaqueline.natario@gmail.com>
2021-08-30 19:30:10 -0300

    Pratica DOM e Seletores

---
+++ 1-fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/pratica3.js
@@ -0,0 +1,13 @@
+document.querySelector("#page-title").innerHTML = "Um lugar chamado Nothing Hill"
+
+//document.getElementById("paragraph").style.color = "red"
+
+//document.getElementById("paragraph").innerHTML = "Data de lançamento: 30 de julho de 1999 (Brasil"
+
+document.querySelector("#subtitle").innerHTML = "Sinopse"
+
+//document.getElementById("second-paragraph").innerHTML = "William Thacker é dono de uma livraria em Londres. A monotonia de sua existência é tumultuada romanticamente quando a famosa atriz estadunidense Anna Scott aparece em sua loja. Um encontro fortuito sobre um suco de laranja derramado leva a um beijo que dá início a um caso de amor. Conforme ele e a glamorosa estrela de cinema se aproximam cada vez mais, eles lutam para conciliar seus estilos de vida radicalmente diferentes em nome do amor."
+
+document.querySelectorAll(".paragraph")[0].innerHTML = "Praticando a função querySelector e querySelectorAll";
+
+document.querySelector("#subtitle").style.color = "orange";
---
+++ 1-fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/pratica4_adm_tempo_trybe.js
@@ -0,0 +1,11 @@
+document.getElementById("header-container").style.backgroundColor = "rgb(76,164,109)";
+
+document.getElementsByTagName("div")[0].style.backgroundColor = "Coral";
+document.getElementsByTagName("div")[1].style.backgroundColor = "Coral";
+document.getElementsByTagName("div")[2].style.backgroundColor = "yellow";
+document.getElementsByTagName("div")[3].style.backgroundColor = "yellow";
+
+document.getElementsByTagName("h3")[0].style.backgroundColor = "DarkMagenta";
+document.getElementsByTagName("h3")[1].style.backgroundColor = "DarkMagenta";
+document.getElementsByTagName("h3")[2].style.backgroundColor = "black";
+document.getElementsByTagName("h3")[3].style.backgroundColor = "black";
