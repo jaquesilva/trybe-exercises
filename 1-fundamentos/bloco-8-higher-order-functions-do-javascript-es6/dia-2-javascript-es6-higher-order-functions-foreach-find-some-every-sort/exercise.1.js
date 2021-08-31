c9042abda84df688b3bca90c1c83bf0c5b2c0785
Jaqueline Silva <jaqueline.natario@gmail.com>
2021-08-31 10:05:17 -0300

    Prática HOF - forEach, find, some, every e sort

---
+++ 1-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercise.1.js
@@ -0,0 +1,101 @@
+const assert = require('assert');
+
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+
+// Adicione o código do exercício aqui:
+
+// 1 - Encontre o nome da primeira pessoa autora do livro nascida no ano de 1947.
+// Dica: use a função find .
+
+const authorBornIn1947 = () => {
+  const elemento = books.find((book) => book.author.birthYear === 1947);
+  return elemento.author.name;
+}
+
+console.log(authorBornIn1947());
+
+assert.strictEqual(authorBornIn1947(), 'Stephen King');
+
+// 7 - Faça uma função que retorne true , caso nenhum author tenha nascido no mesmo ano, e false , caso contrário.
+
+const expectedResult = false;
+
+const authorUnique = () => {
+  // escreva seu código aqui
+}
+
+assert.strictEqual(authorUnique(), expectedResult);
+
+// const users = [
+//   { firstName: 'Homer', lastName: 'Simpson', isDriver: true },
+//   { firstName: 'Marge', lastName: 'Simpson', isDriver: true },
+//   { firstName: 'Bart', lastName: 'Simpson', isDriver: true },
+//   { firstName: 'Lisa', lastName: 'Simpson', isDriver: true },
+//   { firstName: 'Maggie', lastName: 'Simpson', isDriver: true },
+// ];
+
+
+// const everyOneCanDrive = users.every(user => user.isDriver === true);
+
+// console.log(everyOneCanDrive);
