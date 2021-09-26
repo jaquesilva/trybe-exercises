faf0032bcdbdc91ae7eda5a1304f22879045d843
Jaqueline Silva <jaqueline.natario@gmail.com>
2021-09-26 20:59:44 -0300

    Fixation exercises about props

--- 2-desenvolvimento-front-end/bloco-11-introducao-a-react/dia-2-componentes-react/conteudo/fixation-exercises-11-2/src/Image.js
+++ 2-desenvolvimento-front-end/bloco-11-introducao-a-react/dia-2-componentes-react/conteudo/fixation-exercises-11-2/src/Image.js
@@ -11,3 +11,5 @@ export default Image;
 // 1 - Qual o nome do componente declarado acima? R= Image
 
 // Chame o componente Image , de forma que seja mostrada esta imagem, ou o texto Cute cat staring , caso a imagem não consiga ser carregada. R= Vou renderizar o componente "Image.js" dentro de "App.js"
+
+// Uma alteração qualquer pra salvar
