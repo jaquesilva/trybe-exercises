1d7ba2e44603e870cbaecf619afc7dca6d99a364
Jaqueline Silva <jaqueline.natario@gmail.com>
2021-09-26 20:38:07 -0300

    Solves the props fixation exercise

---
+++ 2-desenvolvimento-front-end/bloco-11-introducao-a-react/dia-2-componentes-react/conteudo/fixation-exercises-11-2/src/App.js
@@ -0,0 +1,13 @@
+import staringCat from './staringCat.jpeg';
+import Image from './Image';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Image source={staringCat} alternativeText="Cute cat staring" />
+    </div>
+  );
+}
+
+export default App;
---
+++ 2-desenvolvimento-front-end/bloco-11-introducao-a-react/dia-2-componentes-react/conteudo/fixation-exercises-11-2/src/Image.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Image extends React.Component {
+  render() {
+    return <img src={this.props.source} alt={this.props.alternativeText} />;
+  }
+}
+
+export default Image;
+
+// 1 - Qual o nome do componente declarado acima? R= Image
+
+// Chame o componente Image , de forma que seja mostrada esta imagem, ou o texto Cute cat staring , caso a imagem não consiga ser carregada. R= Vou renderizar o componente "Image.js" dentro de "App.js"
