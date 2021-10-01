abbdf49c1eb9846dea77a986cd55e66718687041
Jaqueline Silva <jaqueline.natario@gmail.com>
2021-10-01 15:53:28 -0300

    Create the 'Order.js' component

--- 2-desenvolvimento-front-end/bloco-11-introducao-a-react/dia-2-componentes-react/conteudo/fixation-exercises-11-2/src/App.js
+++ 2-desenvolvimento-front-end/bloco-11-introducao-a-react/dia-2-componentes-react/conteudo/fixation-exercises-11-2/src/App.js
@@ -1,15 +1,43 @@
-import staringCat from './staringCat.jpeg';
-import Image from './Image';
+import React from 'react';
+// import staringCat from './staringCat.jpeg';
+// import Image from './Image';
 import './App.css';
+import Order from './Order';
 
-function App() {
-  return (
+class App extends React.Component {
+  render() {
+
+    const headphone = {
+      id: 102,
+      user: "cena@gmail.com",
+      product: "Razer Headphone",
+      price: {
+        value: 99.99,
+        currency: "dollars"
+      }
+    };
+
+    const energyDrink = {
+      id: 77,
+      user: "cena@gmail.com",
+      product: "Monster 500mL",
+      price: {
+        value: 9.99,
+        currency: "dollars"
+      }
+    };
+
+    return (
     <div className="App">
-      <Image source={staringCat} alternativeText="Cute cat staring" />
+      {/* <Image source={staringCat} alternativeText="Cute cat staring" /> */}
+      <h1> Orders recently created </h1>
+      <Order order={ headphone }/>
+      <Order order={ energyDrink }/>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
 
-// teste
+// O que o componente App é em relação a Order ? R= O componente 'App.js' é pai do componente 'Order.js'
