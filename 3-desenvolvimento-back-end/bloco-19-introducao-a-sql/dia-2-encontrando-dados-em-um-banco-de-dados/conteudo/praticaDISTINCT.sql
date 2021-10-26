5f23aeca9d770dc491bf92716b7d7d6d2b82bf24
Jaqueline Silva <jaqueline.natario@gmail.com>
2021-10-26 11:42:24 -0300

    Adiciona o arquivo para a prática do DISTINCT

---
+++ 3-desenvolvimento-back-end/bloco-19-introducao-a-sql/dia-2-encontrando-dados-em-um-banco-de-dados/conteudo/praticaDISTINCT.sql
@@ -0,0 +1,17 @@
+-- 1. Monte uma query para encontrar pares únicos de nomes e idades .
+SELECT DISTINCT nome, idade FROM Escola.Alunos;
+
+-- 2. Quantas linhas você encontraria na query anterior?
+-- R = 5
+
+-- 3. Monte uma query para encontrar somente os nomes únicos
+SELECT DISTINCT nome FROM Escola.Alunos;
+
+-- 4. Quantas linhas você encontraria na query anterior?
+-- R = 4
+
+-- 5. Monte uma query para encontrar somente as idades únicas
+SELECT DISTINCT idade FROM Escola.Alunos;
+
+-- 6. Quantas linhas você encontraria na query anterior?
+-- R = 4
