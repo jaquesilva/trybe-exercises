a6b6aa7bb68849e9a148c9e85278aa95a4b46f24
Jaqueline Silva <jaqueline.natario@gmail.com>
2021-10-27 09:52:34 -0300

    Adiciona arquivo para a prática dos operadores booleanos e relacionais

--- 3-desenvolvimento-back-end/bloco-19-introducao-a-sql/dia-3-filtrando-dados-de-forma-específica/conteudo/paraFixarBooleanos.sql
+++ 3-desenvolvimento-back-end/bloco-19-introducao-a-sql/dia-3-filtrando-dados-de-forma-específica/conteudo/paraFixarBooleanos.sql
@@ -18,3 +18,54 @@ SELECT first_name FROM customer
 WHERE active = 0 AND store_id = 2 AND first_name <> 'KENNETH'
 ORDER BY first_name;
 
+-- 3. O setor financeiro quer saber título, descrição, ano de lançamento e valor do custo de 
+-- substituição ( replacement_cost ), dos 100 filmes com o maior custo de substituição, do valor 
+-- mais alto ao mais baixo, entre os filmes feitos para menores de idade e que têm o custo mínimo 
+-- de substituição de $18,00 dólares. Em caso de empate, ordene em ordem alfabética pelo título.
+-- As informações podem ser encontradas na tabela film
+
+-- Minha tentativa:
+SELECT title, description, release_year, replacement_cost FROM sakila.film
+
+ORDER BY DESC
+LIMIT 100
+
+-- Query correta:
+USE sakila;
+SELECT title, description, release_year, replacement_cost FROM film
+WHERE rating <> 'NC-17' AND replacement_cost >= 18.00
+ORDER BY replacement_cost DESC, title
+LIMIT 100;
+
+-- 4. Quantos clientes estão ativos e na loja 1 ? As informações podem ser encontradas na tabela customer
+
+-- Minha tentativa:
+SELECT * FROM sakila.customer
+WHERE active = 1 AND store_id = 1;
+
+-- Query correta:
+USE sakila;
+SELECT COUNT(*) AS quantidade_de_clientes_ativos FROM customer
+WHERE active = 1 AND store_id = 1;
+
+-- 5. Mostre todos os detalhes dos clientes que não estão ativos na loja 1 . As informações podem 
+-- ser encontradas na tabela customer
+SELECT * FROM sakila.customer
+WHERE active = 0 AND store_id = 1;
+
+-- 6. Precisamos descobrir quais são os 50 filmes feitos apenas para adultos com a menor taxa de 
+-- aluguel, para que possamos fazer uma divulgação melhor desses filmes. Em caso de empate, ordene em 
+-- ordem alfabética pelo título. As informações podem ser encontradas na tabela film
+
+-- Minha tentativa:
+SELECT * FROM sakila.film
+WHERE rating = 'NC-17' AND rental_rate <= 0.99
+ORDER BY title ASC
+LIMIT 50;
+
+-- Query correta:
+USE sakila;
+SELECT title FROM film
+WHERE rating = 'NC-17' 
+ORDER BY rental_rate, title
+LIMIT 50;
