6e42c25d561e1c642615e744ecfaf2ab64ef1bcb
Jaqueline Silva <jaqueline.natario@gmail.com>
2022-01-11 18:12:03 -0400

    Trabalhando com Mocha, Chai e Sinon

---
+++ 3-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-3-mocha-chai-e-sinon-testes-de-back-end-com-node-js/conteudo/examples2/io-test/leArquivo.js
@@ -0,0 +1,19 @@
+// module.exports = () => {
+//   //
+// }
+
+// ============================
+
+const fs = require('fs');
+
+function leArquivo(nomeDoArquivo) {
+  try {
+    const conteudoDoArquivo = fs.readFileSync(nomeDoArquivo, 'utf8');
+
+    return conteudoDoArquivo;
+  } catch (err) {
+    return null;
+  }
+}
+
+module.exports = leArquivo;
