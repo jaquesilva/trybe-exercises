d8c747ea0d6b0ef69e88aa3af9fb9090c1b876e6
Jaqueline Maria da Silva <80333540+jaquesilva@users.noreply.github.com>
2022-02-14 18:08:00 -0300

    Delete 3-desenvolvimento-back-end/bloco-24-node-js-orm-e-autenticacao/dia-1-introducao-node-js-orm-e-autenticacao-orm-interface-da-aplicacao-com-o-banco-de-dados directory

--- 3-desenvolvimento-back-end/bloco-24-node-js-orm-e-autenticacao/dia-1-introducao-node-js-orm-e-autenticacao-orm-interface-da-aplicacao-com-o-banco-de-dados/conteudo/app-with-sequelize/models/user.js
+++
@@ -1,9 +0,0 @@
-const User = (sequelize, DataTypes) => {
-  const User = sequelize.define("User", {
-    fullName: DataTypes.STRING,
-    email: DataTypes.STRING,
-  });
-  return User;
-};
-
-module.expors = User;
