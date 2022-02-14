bf492b5435c793e386e68aa3194600310771b3a6
Jaqueline Silva <jaqueline.natario@gmail.com>
2022-02-14 17:43:42 -0300

    Adiciona exercícios para a prática da camada Model

---
+++ 3-desenvolvimento-back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-introducao-arquitetura-de-software-e-arquitetura-de-software-camada-de-model/conteudo/model-example/models/connection.js
@@ -0,0 +1,14 @@
+    // models/connection.js
+
+    const mysql = require('mysql2/promise'); //importamos o mysql do módulo mysql2/promise , assim utilizamos a versão mais atualizada do mysql2 em vez de usar a versão com callbacks
+
+    const connection = mysql.createPool({ // O método createPool cria uma pool de conexões com o banco de dados. Isso significa que a própria biblioteca irá gerenciar as múltiplas conexões que fizermos com o banco. O createPool recebe um objeto com as credenciais necessárias para estabelecer a conexão. Entre as opções possíveis, estão:
+
+        host: 'localhost', // local onde o servidor do MySQL está armazenado. Como estamos executando localmente, usamos localhost
+        user: 'trybe', // usuário que vamos utilizar para acessar o banco
+        password: 'trybe2021', // senha do usuário especificado
+        database: 'model_example' }); // nome do banco com o qual queremos nos conectar
+
+    module.exports = connection;
+
+// O método createPool retorna um objeto Pool representando uma sessão com o banco. Para não ser necessário criar uma sessão e selecionar o schema sempre que precisarmos acessar o banco, armazenamos nossa pool na variável connection .
