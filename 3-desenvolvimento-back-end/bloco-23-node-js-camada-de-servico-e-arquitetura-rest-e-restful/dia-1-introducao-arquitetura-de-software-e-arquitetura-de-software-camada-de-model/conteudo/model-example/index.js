003664bccf53b87d9dc252581af43d86909e530c
Jaqueline Silva <jaqueline.natario@gmail.com>
2022-02-16 15:19:00 -0300

    Cria a rota /books/:id

--- 3-desenvolvimento-back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-introducao-arquitetura-de-software-e-arquitetura-de-software-camada-de-model/conteudo/model-example/index.js
+++ 3-desenvolvimento-back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-introducao-arquitetura-de-software-e-arquitetura-de-software-camada-de-model/conteudo/model-example/index.js
@@ -7,17 +7,38 @@ const app = express();
 
 // ===================================
 
-app.get('/authors', async (_req, res) => {
+app.get('/authors', async (req, res) => {
   const authors = await Author.getAll();
 
   res.status(200).json(authors);
 });
 
-app.get('/books', async (_req, res) => {
-  const books = await Book.getAll();
+app.get('/authors/:id', async (req, res) => {
+  const { id } = req.params;
+  const author = await Author.findById(id);
+
+  if (!author) return res.status(404).json({ message: 'Not found' })
+
+  res.status(200).json(author);
+})
+
+app.get('/books', async (req, res) => {
+  const { author_id } = req.query;
+  const books = (author_id)
+  ? await Book.getByAuthorId(author_id)
+  : await Book.getAll();
 
   res.status(200).json(books);
 });
+
+app.get('/book/:id', async (req, res) => {
+  const { id } = req.params;
+  const book = await Book.getById(id);
+
+  if (!book) return res.status(404).json({ message: 'Not found' })
+
+  res.status(200).json(book);
+})
 
 // ====================================
 
--- 3-desenvolvimento-back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-introducao-arquitetura-de-software-e-arquitetura-de-software-camada-de-model/conteudo/model-example/models/Book.js
+++ 3-desenvolvimento-back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-introducao-arquitetura-de-software-e-arquitetura-de-software-camada-de-model/conteudo/model-example/models/Book.js
@@ -38,7 +38,33 @@ const getAll = async () => {
     authorId: author_id,
   }));
 };
+
+const getByAuthorId = async () => {
+  const query = 'SELECT * FROM model_example.books WHERE author_id=?;'
+  const [books] = await connection.execute(query, [authorId]);
+
+  return books.map(({ id, title, author_id }) => ({
+    id,
+    title,
+    authorId: author_id,
+  }));
+};
+
+const getById = async (id) => {
+  const query = 'SELECT * FROM model_example.books WHERE id=?;'
+  const [books] = await connection.execute(query, [id]);
+
+  if (books.length === 0) return null;
+
+  return books.map(({ id, title, author_id }) => ({
+    id,
+    title,
+    authorId: author_id,
+  }))[0];
+}
   
 module.exports = {
   getAll,
+  getByAuthorId,
+  getById,
 };
