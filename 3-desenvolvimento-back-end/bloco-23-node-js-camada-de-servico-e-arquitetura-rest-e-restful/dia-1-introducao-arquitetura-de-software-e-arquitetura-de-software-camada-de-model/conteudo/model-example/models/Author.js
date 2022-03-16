b773095251cca0963c00890f3bf5ec440f25f1a6
Jaqueline Silva <jaqueline.natario@gmail.com>
2022-03-16 13:24:43 -0300

    learning Model layer

--- 3-desenvolvimento-back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-introducao-arquitetura-de-software-e-arquitetura-de-software-camada-de-model/conteudo/model-example/models/Author.js
+++ 3-desenvolvimento-back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-introducao-arquitetura-de-software-e-arquitetura-de-software-camada-de-model/conteudo/model-example/models/Author.js
@@ -1,40 +1,41 @@
 const connection = require('./connection');
 
-//  // Cria uma string com o nome completo do autor
-//  const getNewAuthor = ({id, firstName, middleName, lastName}) => {
-//   // Note que `Boolean` é uma função que recebe um parâmetro e retorna true ou false
-//   // nesse caso, se middle_name for `undefined` ou uma string vazia o retorno será `false`
-//   const fullName = [firstName, middleName, lastName]
-//     .filter(Boolean)
-//     .join(' ');
+ // Cria uma string com o nome completo do autor:
+ const getNewAuthor = ({id, firstName, middleName, lastName}) => {
+  // Note que `Boolean` é uma função que recebe um parâmetro e retorna true ou false
+  // nesse caso, se middle_name for `undefined` ou uma string vazia o retorno será `false`
+  const fullName = [firstName, middleName, lastName]
+    .filter(Boolean)
+    .join(' ');
 
-//   return {
-//   id,
-//   firstName,
-//   middleName,
-//   lastName,
-//   fullName,
-//  };
-// };
+  return {
+  id,
+  firstName,
+  middleName,
+  lastName,
+  fullName,
+ };
+};
 
 // Outra maneira de fazer a função "getNewAuthor":
-const getNewAuthor = ({ id, firstName, middleName, lastName }) => {
-  // Essa const fullName vai juntar os 3 valores (firstName, middleName e o lastName), no caso colocamos .filter pq tem alguns casos que o autor não tem middleName. O método join transforma o array em string separado por espaço
-  const fullName = [firstName, middleName, lastName].filter((name) => name).join(" ");
+
+// const getNewAuthor = ({ id, firstName, middleName, lastName }) => {
+//   // Essa const fullName vai juntar os 3 valores (firstName, middleName e o lastName), no caso colocamos .filter pq tem alguns casos que o autor não tem middleName. O método join transforma o array em string separado por espaço
+//   const fullName = [firstName, middleName, lastName].filter((name) => name).join(" ");
   
-  return {
-    id,
-    firstName,
-    middleName,
-    lastName,
-    fullName,
-  };
-};
+//   return {
+//     id,
+//     firstName,
+//     middleName,
+//     lastName,
+//     fullName,
+//   };
+// };
 
 
 // ===========================================
 
-// Converte o nome dos campos de snake_case para camelCase
+// Converte o nome dos campos de snake_case para camelCase:
 const serialize = (authorData) => { // de onde vem o "authorData" ?
   return {
     id: authorData.id,
@@ -46,13 +47,23 @@ const serialize = (authorData) => { // de onde vem o "authorData" ?
 
 // ========================================
 
+// Função que retornará todas as pessoas autoras cadastradas no banco de dados
 // Busca todas as pessoas autoras do banco.
 const getAll = async () => {
-const [authors] = await connection.execute(
+const [authors] = await connection.execute( // método execute -> retorna uma Promise que quando resolvida, nos fornece um array com 2 campos: [rows, fields] . O primeiro index é onde está a resposta que desejamos (no caso o Authors) e no segundo vêm algumas informações extras sobre a query que não iremos utilizar.
   'SELECT id, first_name, middle_name, last_name FROM model_example.authors;',
 );
 return authors.map(serialize).map(getNewAuthor);
 };
+
+// Outra forma de fazer:
+
+// const getAll = async () => {
+//   const query = 'SELECT id, first_name, middle_name, last_name FROM model_example.authors;',
+//   const [authors] = await connection.execute(query)
+  
+//   return authors.map(serialize).map(getNewAuthor);
+// };
 
 // =======================================
 
@@ -78,7 +89,7 @@ const getById = async (id) => {
   })
 };
 
-// ========================================
+// // ========================================
 
 // Função que retorna um boolean indicando se os dados são válidos, checando se firstName e lastName são strings não vazias, e se middleName , caso seja informado, é uma string
 const isValid = (firstName, middleName, lastName) => {
