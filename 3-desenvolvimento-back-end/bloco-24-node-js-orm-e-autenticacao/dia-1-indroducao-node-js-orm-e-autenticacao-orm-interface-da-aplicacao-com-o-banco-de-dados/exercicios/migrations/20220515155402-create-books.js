12b576f6e3ecd125e67ed065af4c63cef6db9c68
Jaqueline Silva <jaqueline.natario@gmail.com>
2022-05-15 13:04:53 -0300

    Exercise 1 - done

---
+++ 3-desenvolvimento-back-end/bloco-24-node-js-orm-e-autenticacao/dia-1-indroducao-node-js-orm-e-autenticacao-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicios/migrations/20220515155402-create-books.js
@@ -0,0 +1,58 @@
+'use strict';
+
+// module.exports = {
+//   async up (queryInterface, Sequelize) {
+//     /**
+//      * Add altering commands here.
+//      *
+//      * Example:
+//      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+//      */
+//   },
+
+//   async down (queryInterface, Sequelize) {
+//     /**
+//      * Add reverting commands here.
+//      *
+//      * Example:
+//      * await queryInterface.dropTable('users');
+//      */
+//   }
+// };
+
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Books', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      author: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      pageQuantity: {
+        allowNull: true,
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Books');
+  }
+};
