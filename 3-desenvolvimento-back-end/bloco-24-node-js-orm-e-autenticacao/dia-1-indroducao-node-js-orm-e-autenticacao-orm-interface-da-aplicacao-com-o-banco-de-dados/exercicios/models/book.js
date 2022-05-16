5d28b8c7387a89a5492b9a986d7261d81bbb2d8b
Jaqueline Silva <jaqueline.natario@gmail.com>
2022-05-16 09:08:43 -0300

    Exercise 2 - done

---
+++ 3-desenvolvimento-back-end/bloco-24-node-js-orm-e-autenticacao/dia-1-indroducao-node-js-orm-e-autenticacao-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicios/models/book.js
@@ -0,0 +1,9 @@
+const Book = (sequelize, DataTypes) => {
+  const Book = sequelize.define("Book", {
+    title: DataTypes.STRING,
+    author: DataTypes.STRING,
+    pageQuantity: DataTypes.INTEGER,
+    createdAt: DataTypes.DATE,
+    updateat: DataTypes.DATE
+  })
+}
