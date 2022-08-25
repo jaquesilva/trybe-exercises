de835773f81a91dfe9e32c2467bb212c78dad100
Jaqueline Silva <jaqueline.natario@gmail.com>
2022-08-25 14:47:04 -0300

    feat: exercício 3 - conteúdo - operações básicas

---
+++ 4-ciencia-da-computacao/bloco-33-introducao-a-python/dia-1-introducao-ciencia-da-computacao-aprendendo-python/conteudo/exercicio3.py
@@ -0,0 +1,9 @@
+# Teste e verifique o que acontece se você colocar um ponto e vírgula no final de uma instrução em Python.
+
+hours = 6;
+
+# Quantos minutos tem 6h?
+minutes = hours * 60;
+
+# Obs: Não aconteceu nada :|
+# Assim como em JavaScript, Python permite que você coloque um ponto e vírgula no final de uma instrução.
