e15ec6c04a89fa59b4664e5ac010a211088707af
Jaqueline Silva <jaqueline.natario@gmail.com>
2022-08-25 14:34:42 -0300

    feat: exercícios - conteúdo - operações básicas

---
+++ 4-ciencia-da-computacao/bloco-33-introducao-a-python/dia-1-introducao-ciencia-da-computacao-aprendendo-python/conteudo/exercicio1.py
@@ -0,0 +1,23 @@
+a = 10
+b = 5
+
+# soma
+print(a + b)
+
+# subtração
+print(a - b)
+
+# multiplicação
+print(a * b)
+
+# divisão
+print(a / b)
+
+# divisão inteira
+print(a // b)
+
+# potenciação
+print( a ** b)
+
+# módulo
+print(a % b)
