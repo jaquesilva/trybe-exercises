5a7ce50128977fb243abd0b23e596b689cb3fc8b
Jaqueline Silva <jaqueline.natario@gmail.com>
2022-09-27 14:15:04 -0300

    feat: while + input em Python

--- 4-ciencia-da-computacao/bloco-33-introducao-a-python/dia-2-entrada-e-saida-de-dados/conteudo/somador_de_numeros.py
+++ 4-ciencia-da-computacao/bloco-33-introducao-a-python/dia-2-entrada-e-saida-de-dados/conteudo/somador_de_numeros.py
@@ -1,7 +1,16 @@
 # input
 
-meu_numero = int(input("Digite seu número:"))
+# meu_numero = int(input("Digite seu número:"))
 
-print(type(meu_numero))
-print(meu_numero)
-print(meu_numero + 1)
+# print(type(meu_numero))
+# print(meu_numero)
+# print(meu_numero + 1)
+
+# -----------------------------------------------
+
+meu_numero = 0
+
+while meu_numero < 42:
+    meu_numero += int(input("Digite seu número:"))
+
+print("A soma de seus números superou 42")
