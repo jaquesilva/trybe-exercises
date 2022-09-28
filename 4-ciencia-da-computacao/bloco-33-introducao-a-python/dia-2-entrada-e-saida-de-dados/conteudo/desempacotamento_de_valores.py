04fad6222663bb49fcd087b0a678c10ed0004d2b
Jaqueline Silva <jaqueline.natario@gmail.com>
2022-09-28 13:36:30 -0300

    feat: Desempacotamento de valores

---
+++ 4-ciencia-da-computacao/bloco-33-introducao-a-python/dia-2-entrada-e-saida-de-dados/conteudo/desempacotamento_de_valores.py
@@ -0,0 +1,10 @@
+
+a, b, c = "def"
+print(a)  # saída: d
+print(b)  # saída: e
+print(c)  # saída: f
+
+head, *tail = (1, 2, 3)  # Quando um * está presente no desempacotamento,
+# os valores são desempacotados em formato de lista.
+print(head)  # saída: 1
+print(tail)  # saída: [2, 3]
